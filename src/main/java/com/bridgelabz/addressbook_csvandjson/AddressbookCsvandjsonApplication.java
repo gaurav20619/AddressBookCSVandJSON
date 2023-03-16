332f428e6705eb05e46c34f7f494f0c700fbdbfd
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-03-16 20:59:50 +0530

    Add: first commit

---
+++ src/main/java/com/bridgelabz/addressbook_csvandjson/AddressbookCsvandjsonApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbook_csvandjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbookCsvandjsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressbookCsvandjsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/addressbook_csvandjson/AddressbookCsvandjsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbook_csvandjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbookCsvandjsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
